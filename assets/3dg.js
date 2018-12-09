430864e8e2b70daaad338861cdb957bdd3e7bf90
Austin <awtotty@gmail.com>
2018-12-08 22:52:37 -0800

    Explore mathquill

--- assets/3dg.js
+++ assets/3dg.js
@@ -1,18 +1,40 @@
 /*
- * Code for getting and using user form data to create and display a 3d graph 
- * of a function on the page. 
+ * Code for getting and using user form data to create and display a 3d graph
+ * of a function on the page.
  */
+// import mathquill-0.10.0/mathquill.js
+
 
 // Default function string for initial display
 var fString = "Math.sin(x/50)*Math.cos(y/50)*50 + 50"; // fString is the string version of the input function
 var useDefaultFunction = true;	// boolen for using default fString
 
 // Vars for axis params and density of mesh
-var axisMin, axisMax, steps; 
+var axisMin, axisMax, steps;
 var stepCap = 150; // maximum number of steps allowed to prevent performance issues
 
 var typingTimer;                // timer identifier
 var doneTypingInterval = 1500;  // time in ms, used to time pause between typing and graphing
+
+
+
+
+// Start mathquill tests
+// TODO figure this shit out...
+var MQ = MathQuill.getInterface(2);
+
+var answerSpan = document.getElementById('answer');
+var answerMathField = MQ.MathField(answerSpan, {
+  handlers: {
+    edit: function() {
+      var enteredMath = answerMathField.latex(); // Get entered math in LaTeX format
+      // checkAnswer(enteredMath); // TODO this is throwing an error.
+    }
+  }
+});
+
+// end mathquill
+
 
 // Used on keyup, start the countdown
 // after doneTypingInterval, draw graph with given function
@@ -31,26 +53,26 @@ function setFunction() {
   if (!useDefaultFunction)
     fString = document.getElementById("fname").value;
   // console.log(new Function("x","y", "return " + fString));
-  
+
   // if default funciton has been used once, don't use it again
-  useDefaultFunction = false; 
+  useDefaultFunction = false;
 }
 
 function setParams() {
   // if any of these is not defined, use default values
   axisMin = document.getElementById("axisMin").value
   if (axisMin === "" || isNaN(axisMin))
-    axisMin = 0; 
- 
+    axisMin = 0;
+
   axisMax = document.getElementById("axisMax").value
   if (axisMax === "" || isNaN(axisMax))
-    axisMax = 314; 
-	
+    axisMax = 314;
+
   // number of data points will be steps*steps
   steps = document.getElementById("steps").value
   if (steps === "" || isNaN(steps))
     steps = 25;
-  if (steps > stepCap) // TODO tell user about cap and actual steps val 
+  if (steps > stepCap) // TODO tell user about cap and actual steps val
     steps = stepCap
 }
 
@@ -62,29 +84,29 @@ function f(x,y) {
   try {
     var func = new Function("x","y", "return " + fString + ";");
     return func(x,y);
-  } catch(err) { // TODO move catch to graph and break loop so default graph displays? 
-    return 0;	// TODO but then a single undefined point on the graph will kill the graph.  
+  } catch(err) { // TODO move catch to graph and break loop so default graph displays?
+    return 0;	// TODO but then a single undefined point on the graph will kill the graph.
   }
 }
 
 // Creates the graph of the function. Called after the timer reaches doneTypingInterval
 function graph() {
-  // update function string 
+  // update function string
   setFunction();
 
   // get params from user
-  setParams(); 
+  setParams();
 
-  var counter = 0; // creates ids for the points 
+  var counter = 0; // creates ids for the points
   var axisStep = axisMax / steps;
-  
-  // create blank data set to populate with point data 
+
+  // create blank data set to populate with point data
   var data = new vis.DataSet();
-  
+
   // build data points
   for (var x = 0; x < axisMax; x+=axisStep) {
       for (var y = 0; y < axisMax; y+=axisStep) {
-          var z = f(x,y); //  
+          var z = f(x,y); //
           data.add({id:counter++, x:x, y:y, z:z, style:z});
       }
   }
